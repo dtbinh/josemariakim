43a6059d4ca0511c1528a9146c17a59b807086b9
bjergekim <bjergekim@6ede59ef-229f-254a-928b-1c053548990a>
2011-01-03 12:46:44 +0000

    Changed position of robot arm

--- FP_R2_Comm/src/SeqStrategy.java
+++ FP_R2_Comm/src/SeqStrategy.java
@@ -25,7 +25,7 @@ public class SeqStrategy extends Behavior
    
     public void liftGripArm()
     {
-		gripper.rotateTo(30);
+		gripper.rotateTo(-50);
     }
 
     private void releaseGripArm()
@@ -35,7 +35,7 @@ public class SeqStrategy extends Behavior
 
     private void lowerGripArm()
     {
-		gripper.rotateTo(0);
+		gripper.rotateTo(-15);
     }
     
 	private boolean WaitMoving()
